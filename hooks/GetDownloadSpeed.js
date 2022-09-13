a2e409222747d3f3a4a5c1c5a1af666786821dd1
ZZCWC <85448514+losier@users.noreply.github.com>
2022-09-13 10:30:57 +0530

    added download speed

--- hooks/GetDownloadSpeed.js
+++ hooks/GetDownloadSpeed.js
@@ -1,9 +1,17 @@
 import { useState } from "react";
 
 import NetworkSpeed from "network-speed";
+const testNetworkSpeed = new NetworkSpeed();
 
 const GetDownloadSpeed = async () => {
+  const baseUrl = "https://eu.httpbin.org/stream-bytes/500000";
+  const fileSizeInBytes = 500000;
+  const speed = await testNetworkSpeed.checkDownloadSpeed(
+    baseUrl,
+    fileSizeInBytes
+  );
 
-}
+  return speed.kbps;
+};
 
 export default GetDownloadSpeed;
