0b343a0f7a67e63528c447580511b7e4a3940395
ZZCWC <85448514+losier@users.noreply.github.com>
2022-09-13 09:50:52 +0530

    added some className

--- components/GetSpeed.js
+++ components/GetSpeed.js
@@ -11,19 +11,19 @@ const GetSpeed = () => {
         <h1>Your Internet speed is:</h1>
         <div className={styles.box}>
           <div className={styles.card}>
-            <article className={styles.card_content}>
+            <article className={`${styles.card_content} ${styles.ping}`}>
               <AiOutlineApi className={styles.card_icons} />
               <h5>Ping</h5>
               <small>13 ms</small>
             </article>
 
-            <article className={styles.card_content}>
+            <article className={`${styles.card_content} ${styles.download}`}>
               <BsDownload className={styles.card_icons} />
               <h5>Download</h5>
               <small>13 mbps</small>
             </article>
 
-            <article className={styles.card_content}>
+            <article className={`${styles.card_content} ${styles.upload}`}>
               <BsUpload className={styles.card_icons} />
               <h5>Upload</h5>
               <small>10 mbps</small>
