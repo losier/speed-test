e8f40131e4638e5938238c9ed0d309d2588c2b29
Thendo <Thendothovhakale0002@gmail.com>
2022-09-21 14:50:03 +0200

    restore files

---
+++ components/GetSpeed.js
@@ -0,0 +1,109 @@
+import React, { useEffect, useState } from "react";
+import styles from "../styles/GetSpeed.module.css";
+
+import { AiOutlineApi } from "react-icons/ai";
+import { BsDownload, BsUpload } from "react-icons/bs";
+
+import NetworkSpeed from "network-speed";
+const testNetworkSpeed = new NetworkSpeed();
+
+/**
+ * @func GetSpeed
+ * @description 'example func'
+ */
+const GetSpeed = () => {
+  
+  const [ping, setPing] = useState(0);
+  const [downloadSpeed, setDownloadSpeed] = useState(0);
+  const [uploadSpeed, setUploadSpeed] = useState(0);
+
+  useEffect(() => {
+    /**
+     * @func getNetworkDownloadSpeed
+     * @description 'example func'
+     */
+
+    async function getNetworkDownloadSpeed() {
+      const baseUrl = "https://eu.httpbin.org/stream-bytes/500000";
+      const fileSizeInBytes = 500000;
+      const speed = await testNetworkSpeed.checkDownloadSpeed(
+        baseUrl,
+        fileSizeInBytes
+      );
+      // console.log(speed);
+      setDownloadSpeed(speed.mbps);
+    }
+
+    /**
+     * @func getPing
+     * @description 'example func'
+     */
+
+    async function getPing() {
+      var startTime = Date.now();
+      await fetch("https://eu.httpbin.org/stream-bytes/500000");
+      var endTime = Date.now();
+
+      var timeDiff = endTime - startTime;
+      // console.log(timeDiff);
+      setPing(timeDiff);
+    }
+
+    /**
+     * @func interval
+     * @description 'example func'
+     * @param ( 'params0'
+     */
+    const interval = setInterval(() => {
+      getNetworkDownloadSpeed();
+      getPing();
+      setUploadSpeed(69);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  /**
+   * @func render
+   * @description 'example func'
+   * @param ( 'params0'
+   */
+  return (
+    <>
+      <div className={styles.network_container}>
+        <h1>Your Internet speed is:</h1>
+        <div className={styles.box}>
+          <div className={styles.card}>
+            <article className={`${styles.card_content} ${styles.ping}`}>
+              <AiOutlineApi className={styles.card_icons} />
+              <h5>Ping</h5>
+              <small>
+                <span className={styles.value}>{ping}</span>
+                ms
+              </small>
+            </article>
+
+            <article className={`${styles.card_content} ${styles.download}`}>
+              <BsDownload className={styles.card_icons} />
+              <h5>Download</h5>
+              <small>
+                <span className={styles.value}>{downloadSpeed}</span>
+                mbps
+              </small>
+            </article>
+
+            <article className={`${styles.card_content} ${styles.upload}`}>
+              <BsUpload className={styles.card_icons} />
+              <h5>Upload</h5>
+              <small>
+                <span className={styles.value}>{uploadSpeed}</span>
+                mbps
+              </small>
+            </article>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default GetSpeed;
