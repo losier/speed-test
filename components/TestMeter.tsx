05cf7cda9f41a325f1e3531eb27fae9ba66186b0
ZZCWC <85448514+losier@users.noreply.github.com>
2022-11-06 19:19:31 +0530

    changed some format

--- components/TestMeter.tsx
+++ components/TestMeter.tsx
@@ -9,24 +9,32 @@ const TestMeter = () => {
   const [download, setDownload] = useState(0);
   const [upload, setUpload] = useState(0);
 
+  const getPing = () => {
+    const startTime = Date.now();
+
+    const endTime = Date.now();
+
+    setPing(endTime - startTime);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.meter_card}>
         <article className={styles.meter}>
           <BsSpeedometer2 className={styles.card_icons} />
-          <h2>test</h2>
+          <h2>{ping} ms</h2>
           <small>Ping</small>
         </article>
 
         <article className={styles.meter}>
           <BsDownload className={styles.card_icons} />
-          <h2>test</h2>
+          <h2>{download}</h2>
           <small>Download</small>
         </article>
 
         <article className={styles.meter}>
           <BsUpload className={styles.card_icons} />
-          <h2>test</h2>
+          <h2>{upload}</h2>
           <small>Upload</small>
         </article>
       </div>
